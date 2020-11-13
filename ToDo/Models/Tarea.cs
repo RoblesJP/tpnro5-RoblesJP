f340b00bf8f1155026771c63248adeda8875b16d
RoblesJP <juan.robles1417@gmail.com>
2020-11-13 18:19:31 -0300

    Agregar clase Tarea.cs

---
+++ ToDo/Models/Tarea.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public enum EstadoTarea
+    {
+        Pendiente,
+        Realizada
+    }
+
+    public class Tarea
+    {
+        private static int nextId = 0;
+        private int id;
+        private string descripcion;
+        private int duracion;
+        private EstadoTarea estado;
+
+        public int Id { get => id; set => id = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Duracion { get => duracion; set => duracion = value; }
+        public EstadoTarea Estado { get => estado; set => estado = value; }
+
+
+
+        public Tarea(string descripcion, int duracion)
+        {
+            Id = ++nextId;
+            Descripcion = descripcion;
+            Duracion = duracion;
+            Estado = EstadoTarea.Pendiente;
+        }
+
+        public void Realizar()
+        {
+            Estado = EstadoTarea.Realizada;
+        }
+    }
+}

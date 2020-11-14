d8759c4f9e08f8942a4e41a2c17e46256a0ee76b
RoblesJP <juan.robles1417@gmail.com>
2020-11-14 20:32:15 -0300

    Intentando hacer el alta de una Tarea

--- ToDo/Controllers/TareasController.cs
+++ ToDo/Controllers/TareasController.cs
@@ -3,21 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ToDo.Models;
+using ToDo.Entidades;
 
 namespace ToDo.Controllers
 {
     public class TareasController : Controller
     {
+        private static List<Tarea> ListaDeTareas = new List<Tarea>();
         public IActionResult Index()
         {
-            List<Tarea> ListaDeTareas = new List<Tarea>();
-            Tarea t1 = new Tarea("Tarea 1", 10);
-            Tarea t2 = new Tarea("Tarea 2", 7);
-            Tarea t3 = new Tarea("Tarea 3", 18);
-            ListaDeTareas.Add(t1);
-            ListaDeTareas.Add(t2);
-            ListaDeTareas.Add(t3);
             return View(ListaDeTareas);
         }
     }

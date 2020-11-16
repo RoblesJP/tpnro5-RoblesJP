a2e2f457d6fdd2b0434fc99a8a3280085ef22d59
RoblesJP <juan.robles1417@gmail.com>
2020-11-16 20:51:54 -0300

    Realizado CRUD

--- ToDo/Controllers/TareasController.cs
+++ ToDo/Controllers/TareasController.cs
@@ -10,9 +10,59 @@ namespace ToDo.Controllers
     public class TareasController : Controller
     {
         private static List<Tarea> ListaDeTareas = new List<Tarea>();
+
         public IActionResult Index()
         {
             return View(ListaDeTareas);
+        }
+
+        public IActionResult CargarTareaForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CargarTarea(Tarea nuevaTarea)
+        {
+            if (ModelState.IsValid)
+            {
+                ListaDeTareas.Add(nuevaTarea);
+                return View("Index", ListaDeTareas);
+            } 
+            else
+            {
+                string mensaje = "Error al crear la tarea";
+                return Content(mensaje);
+            }
+            
+            
+        }
+
+        public IActionResult RealizarTarea(int id)
+        {
+            ListaDeTareas.Find(t => t.Id == id).Realizar();
+            return View("Index", ListaDeTareas);
+        }
+
+        public IActionResult BorrarTarea(int id)
+        {
+            ListaDeTareas.RemoveAll(t => t.Id == id);
+            return View("Index", ListaDeTareas);
+        }
+
+        public IActionResult ModificarTareaForm(int id)
+        {
+            Tarea laTarea = ListaDeTareas.Find(t => t.Id == id);
+            return View(laTarea);
+        }
+
+        [HttpPost]
+        public IActionResult ModificarTarea(int id, string Descripcion, int Duracion) 
+        {
+            Tarea laTarea = ListaDeTareas.Find(t => t.Id == id);
+            laTarea.Descripcion = Descripcion;
+            laTarea.Duracion = Duracion;
+            return RedirectToAction("Index", "Tareas");
         }
     }
 }
--- ToDo/Entidades/Tarea.cs
+++ ToDo/Entidades/Tarea.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,11 +21,18 @@ namespace ToDo.Entidades
         private EstadoTarea estado;
 
         public int Id { get => id; set => id = value; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
         public string Descripcion { get => descripcion; set => descripcion = value; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        [Range(1, 100, ErrorMessage = "La duracion debe ser entre 1 y 100")]
         public int Duracion { get => duracion; set => duracion = value; }
         public EstadoTarea Estado { get => estado; set => estado = value; }
 
-
+        public Tarea() 
+        {
+            Id = ++nextId;
+            Estado = EstadoTarea.Pendiente;
+        }
 
         public Tarea(string descripcion, int duracion)
         {
